077f827021fbdec6708cfae9bd1297abdc060d8e
AlekseiYuzhanin <Zombehalk@gmail.com>
2023-09-19 00:08:44 +0700

    Adding all controllers

--- Controllers/PersonController.cs
+++ Controllers/PersonController.cs
@@ -63,5 +63,87 @@ public class PersonController: ControllerBase{
         return Ok(persons);
     }
 
-    
+    [HttpGet("{id}")]
+    public ActionResult<PersonWithSkillsDto> GetPersonById(int id)
+    {
+        var person = _db.Person
+            .Include(p => p.Skills)
+            .Where(p => p.Id == id)
+            .Select(p => new PersonWithSkillsDto
+            {
+                Name = p.Name,
+                DisplayName = p.DisplayName,
+                Skills = p.Skills.Select(s => new SkillDto
+                {
+                    Name = s.Name,
+                    Level = s.Level
+                }).ToList()
+            })
+            .FirstOrDefault();
+
+        if (person == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(person);
+    }
+
+    [HttpPut("{id}")]
+public ActionResult<PersonWithSkillsDto> UpdatePersonById(int id, PersonWithSkillsDto updatedPerson)
+{
+    var person = _db.Person
+        .Include(p => p.Skills)
+        .FirstOrDefault(p => p.Id == id);
+
+    if (person == null)
+    {
+        return NotFound();
+    }
+
+    person.Name = updatedPerson.Name;
+    person.DisplayName = updatedPerson.DisplayName;
+
+    foreach (var skillDto in updatedPerson.Skills)
+    {
+        var existingSkill = person.Skills.FirstOrDefault(s => s.Name == skillDto.Name);
+
+        if (existingSkill != null)
+        {
+            existingSkill.Level = skillDto.Level;
+        }
+        else
+        {
+            var newSkill = new Skill
+            {
+                Name = skillDto.Name,
+                Level = skillDto.Level
+            };
+            person.Skills.Add(newSkill);
+        }
+    }
+
+        _db.SaveChanges();
+
+        return Ok(updatedPerson);
+    }
+
+    [HttpDelete("{id}")]
+    public ActionResult DeletePersonById(int id)
+    {
+        var person = _db.Person
+            .FirstOrDefault(p => p.Id == id);
+
+        if (person == null)
+        {
+            return NotFound();
+        }
+
+        _db.Person.Remove(person);
+        _db.SaveChanges();
+
+        return Ok();
+}
+
+
 }
